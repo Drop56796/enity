fdf10753eacbe90ca867c84d5aacfae75df13d28
seek lol.lua <162717999+Drop56796@users.noreply.github.com>
2024-06-27 19:26:31 +0800

    Update q.lua

--- q.lua
+++ q.lua
@@ -3,7 +3,7 @@ local spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/Regul
 local entity = spawner.Create({
 	Entity = {
 		Name = "G-95",
-		Asset = "?raw=true",
+		Asset = "https://github.com/Drop56796/enity/blob/main/HD.rbxm?raw=true",
 		HeightOffset = 0
 	},
 	Lights = {
